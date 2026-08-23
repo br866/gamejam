ef6ecbff678e74835825933e19594e26fc86d531
gnd010 <goodnailsday@outlook.com>
2026-08-23 21:16:10 +0800

    Aug23audio
    more on the way.

---
+++ UnityProject/UnityProject_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_CRATE_PUSH = 470824146U;
+        static const AkUniqueID PLAY_FOOTSTEP_BRUTEDOC = 3546231729U;
+        static const AkUniqueID PLAY_FOOTSTEP_DOG = 1441942263U;
+        static const AkUniqueID PLAY_FOOTSTEP_HUMAN = 1846914642U;
+        static const AkUniqueID PLAY_KEY_PICKUP = 2881789206U;
+        static const AkUniqueID PLAY_MUS_GAMEPLAY = 2201569778U;
+        static const AkUniqueID STOP_CRATE_PUSH = 708504560U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ANXIETY = 4143496951U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
